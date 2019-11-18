c80e2fe0a57de148115d50f06c40d03a424be0b1
tangkc <1016771049@qq.com>
2019-11-18 21:47:55 +0800

    feign消费者

--- hello-spring-cloud-alibaba/hello-spring-cloud-alibaba-nacos-consumer/src/main/java/com.funtl.hello.spring.cloud.alibaba.nacos.consumer/NacosConsumerApplication.java
+++ hello-spring-cloud-alibaba/hello-spring-cloud-alibaba-nacos-consumer/src/main/java/com.funtl.hello.spring.cloud.alibaba.nacos.consumer/NacosConsumerApplication.java
@@ -3,9 +3,11 @@ package com.funtl.hello.spring.cloud.alibaba.nacos.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient//服务消费者
+@EnableFeignClients//开启feign
 public class NacosConsumerApplication {
 
     public static void main(String [] args){
